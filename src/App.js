33aa03c728b038eb6099872a89549fd419573c75
saremipegah <saremipegah@gmail.com>
2022-07-24 09:44:02 +0430

    pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+
+const [todos,setTodos]=useState([]);
+const [ currentPage ,setCurrentPage]=useState(1)
+const [paginatedTodos , setPaginatedTodos]=useState([])
+
+let pageSize = 10;
+let pagesNumbers;
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(res => res.json())
+  .then(datas =>{
+    setTodos(datas)
+    let endIndex = pageSize * currentPage
+    let startIndex = endIndex - pageSize
+    let allShowTodos= datas.slice(startIndex , endIndex)
+    setPaginatedTodos(allShowTodos)
+  })
+},[])
+
+const changePaginate = (newPage)=>{
+ setCurrentPage(newPage)
+    let endIndex = pageSize * currentPage
+    let startIndex = endIndex - pageSize
+    let allShowTodos= todos.slice(startIndex , endIndex)
+    setPaginatedTodos(allShowTodos)
+}
+
+const pagesCount =Math.ceil(todos.length/pageSize)
+pagesNumbers =Array.from(Array(pagesCount).keys())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src="logogreen.png" className='img' alt="logo" />
+      {!todos ? "loading" : (
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedTodos.map(todo =>(
+              <tr>
+                <td>{todo.id}</td>
+                <td>{todo.userId}</td>
+                <td>{todo.title}</td>
+                <td>
+                  <p className={todo.completed ? "btn btn-success" : "btn btn-danger"}>
+                    {todo.completed ? 'Completed' : 'Pending'}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <nav className="d-flex justify-content-center">
+       <ul className="pagination" aria-current="page">
+        {pagesNumbers.map((pagesNumber)=>(
+          <li style={{cursor:'pointer'}} className={
+            pagesNumber+1 === currentPage 
+            ? "page-item active"
+            : "page-item"
+            }
+            key={pagesNumber+1}
+            onClick={() => changePaginate(pagesNumber+1)}
+            >
+              <span className='page-link'>{pagesNumber+1}</span>
+          </li>
+        ))}
+       </ul>
+      </nav>
     </div>
   );
 }
-
-export default App;
